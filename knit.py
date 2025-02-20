41a1788cce167e661c8b6d57ed1195278223e9ac
Jessica Liu <jessicaliu@Jessicas-MacBook-Air.local>
2025-02-20 03:00:45 -0800

    finished basic pattern

--- knit.py
+++ knit.py
@@ -1,5 +1,5 @@
 def add_suffix(num):
-    if num % 100 in [11, 12, 13]:  # special cases for 11th, 12th, 13th
+    if num % 100 in [11, 12, 13]: 
         suffix = "th"
     else:
         suffix_dict = {1: "st", 2: "nd", 3: "rd"}
@@ -35,8 +35,10 @@ def tube_top_pattern():
     bust_incr_interval = high_bust_to_bust_rows / (bust_incr/4)
 
     waist_decr = bust_sts - waist_sts
-    wait_decr_interval = bust_to_waist_rows / (waist_decr/4)
-    
+    waist_decr_interval = bust_to_waist_rows / (waist_decr/4)
+
+    hip_incr = hip_sts - waist_sts
+    hip_incr_interval = waist_to_hip_rows / (hip_incr/4)
     
     pattern = f"""
     Top-Down Tube Top Pattern:
@@ -44,11 +46,13 @@ def tube_top_pattern():
     2. Join in the round and place a stitch marker. You should now have {int(high_bust_sts)} stitches. 
     3. Knit in the round for {int(bust_incr_interval - 1)} rows.
     4. On the {add_suffix(int(bust_incr_interval))} row, K1, M1L, then K until 1 stitch before the stitch marker, M1R, K2, M1L, then K until 1 stitch before the stitch marker and M1R.
-    5. Repeat steps 3 and 4 until you have knit {int(high_bust_to_bust_rows)} rows and around {int(bust_sts)} stitches.
-    4. Then, begin knitting in the round for {bust_to_waist_rows} rows, decreasing.
-    5. Decrease to {waist_sts} stitches for the waistline.
-    6. Knit for {waist_to_hip_rows} more rows.
-    7. Increase to {hip_sts} stitches until the length reaches the hip line.
+    5. Repeat steps 3 and 4 until you have knit {int(high_bust_to_bust_rows)} rows and adjust your stitch count to {int(bust_sts)} stitches.
+    6. Then, knit in the round for {int(waist_decr_interval - 1)} rows.
+    7. On the next row, K1, SSK, then K until 3 stitches before the stitch marker, K2TOG, K2, SSK, then K until 3 before the stitch marker. K2TOG, K1.
+    8. Repeat steps 6 and 7 until you have {int(high_bust_to_bust_rows + bust_to_waist_rows)}, and adjust your stitch count to {int(waist_sts)} stitches.
+    9. Knit for {int(waist_to_hip_rows - 1)} rows.
+    10. On the next row, K1, SSK, then K until 3 stitches before the stitch marker, K2TOG, K2, SSK, then K until 3 before the stitch marker. K2TOG, K1.
+    11. Repeat steps 9 and 10 until you have {int(high_bust_to_bust_rows + bust_to_waist_rows + waist_to_hip_rows)} rows, or until it reaches the desired length.
     8. Bind off all stitches and weave in all ends.
     """
     print(pattern)
